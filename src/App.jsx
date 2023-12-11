2785d00e7530298b2c499de7fdc760af379b012b
Alexanie <ocxigin@gmail.com>
2023-12-11 21:00:29 +0100

    first commit- folder structure setup

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
+
+export default function App() {
+    return (
+        <div className="text-red-500">This is a new project</div>
+    )
+}
